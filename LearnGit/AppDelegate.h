78478b5fab5ca39f544bc489961b34468ede7c10
caiqiang <caiqiang@kuaijiankang.com>
2017-07-22 10:05:48 +0800

    Initial Commit

---
+++ LearnGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LearnGit
+//
+//  Created by 蔡强 on 2017/7/22.
+//  Copyright © 2017年 kuaijiankang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LearnGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LearnGit
+//
+//  Created by 蔡强 on 2017/7/22.
+//  Copyright © 2017年 kuaijiankang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
